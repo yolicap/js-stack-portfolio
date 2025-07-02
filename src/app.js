e9582bf4bb1d4733c805b3cec80fefe168094626
yolicap <zsalgado3@gatech.edu>
2025-07-02 16:15:33 -0400

    in w the new, out w the old
    new :
    - witchhazel css adapts witch hazel theme to css
    - witchhazel has some niche changes that need to put into their own stylesheet rip
    - new navbar from bootstrap
    - ubuntu mono source
    - base.ejs is the template for all pages now
    - content pages will be in partials for now
    
    old:
    - removed some old template files
    - pretty much it i guess

--- src/app.js
+++ src/app.js
@@ -6,10 +6,11 @@ const ejs = require('ejs');
 const people = ['geddy', 'neil', 'alex'];
 const html = ejs.render('<%= people.join(", "); %>', {people: people});
 
+app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index", {title: "Home"});
+    res.render("base", {content: "./partials/index-content.ejs", title: ""});
 });
 
 app.post('/', (req, res) => {
